bff6e246fa6edd83902b8e680d9a23b43968d6bb
Nugasnakarmi <damnedthinker@gmail.com>
2019-06-25 21:28:19 +0545

    text visible in cards

--- app/src/main/java/s/to/touristnepalapp/MainActivity.java
+++ app/src/main/java/s/to/touristnepalapp/MainActivity.java
@@ -1,9 +1,9 @@
 package s.to.touristnepalapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
+import androidx.cardview.widget.CardView;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
